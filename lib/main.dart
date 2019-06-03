fb6c9ecb4e71e7b85a7b846b79bb4e6552c9e9f4
mamenesia <dedy.prasetyo.h@gmail.com>
2019-06-03 23:17:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// Flutter code sample for material.Scaffold.1
+
+// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
+// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
+// to center the text within the [Scaffold] and the [FloatingActionButton] is
+// centered and docked within the [BottomAppBar] using
+// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
+// connected to a callback that increments a counter.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
